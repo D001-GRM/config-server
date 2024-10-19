8de22f89f730af353f9b7db270732d098082af91
Jerson-Miranda-BBVA <jersonrai.miranda@bbva.com>
2024-10-19 10:23:51 -0600

    Initial commit

---
+++ src/main/java/org/bbva/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.bbva.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
